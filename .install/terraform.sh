0168401a46947b14fe2f7283edb956390f0a2d2b
melvinlee <melvinlee@hotmail.com>
2019-12-09 12:44:42 +0800

    disabled curl progress bar.

--- .install/terraform.sh
+++ .install/terraform.sh
@@ -7,7 +7,7 @@ echo
 echo "Installing terraform ${TERRAFORM_VERSION}..."
 echo
 
-curl -LO https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+curl -LOs https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
 && unzip -d /usr/local/bin terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
 && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
